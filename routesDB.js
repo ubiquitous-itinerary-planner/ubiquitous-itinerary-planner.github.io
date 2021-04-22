8a540196d3d87998fbc48f2431d32e8c588c92f4
erla2996 <erik.larsson.2996@student.uu.se>
2021-04-22 14:18:59 +0200

    added some values for testing map functions

--- routesDB.js
+++ routesDB.js
@@ -6,11 +6,39 @@
 let RDB = {
     "routes" :
         [{
-            "id" : "",
-            "start" : "",
-            "destination" : "",
-            "price" : "",
-            "duration" : "",
-            "company" : ""
+            "id" : "r0",
+            "start" : "0", //id
+            "destination" : "1", //id
+            "price" : "1000", // In SD
+            "duration" : "5", // In days
+            "company" : "Solar Travels"
+        },  {
+            "id" : "r1",
+            "start" : "1", //id
+            "destination" : "0", //id
+            "price" : "1000", // In SD
+            "duration" : "5", // In days
+            "company" : "Solar Travels"
+        },  {
+            "id" : "r2",
+            "start" : "2", //id
+            "destination" : "3", //id
+            "price" : "2000", // In SD
+            "duration" : "10", // In days
+            "company" : "Frogstar Travels"
+        },  {
+            "id" : "r3",
+            "start" : "3", //id
+            "destination" : "2", //id
+            "price" : "2000", // In SD
+            "duration" : "10", // In days
+            "company" : "Frogstar Travels"
+        },  {
+            "id" : "r4",
+            "start" : "0", //id
+            "destination" : "2", //id
+            "price" : "15000", // In SD
+            "duration" : "25", // In days
+            "company" : "The Heart of Gold"
         }]
 };
