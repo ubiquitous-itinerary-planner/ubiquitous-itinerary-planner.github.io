5082448948df95d59a8b8c4113db805bb53e39fe
erla2996 <erik.larsson.2996@student.uu.se>
2021-04-27 13:25:23 +0200

    infopanel hidden from start

--- info.js
+++ info.js
@@ -6,15 +6,18 @@
  * we want an input from the planet clicked upon which works as the index fetching info from database.
  */
 
-function infoInit(){
-    items_table = document.getElementById('info_table');
-    items_table.innerHTML = '';
-    items_table.innerHTML += '<th id="tellus"></th>' +  
-    '<tr><td id="population">' + '</td><td>' + PDB.planets[0].population + '</td></tr>' +
-    '<tr><td id="starsystem">' + '</td><td id="' + PDB.planets[0].starsystem + '">' + '</td></tr>' +
-    '<tr><td id="infrastructure">' + '</td><td>' + PDB.planets[0].infrastructure + '</td></tr>' +
-    '<tr><td id="avgTemp">' + '</td><td>' + PDB.planets[0].meantemp + '</td></tr>' +
-    '<tr><td id="breathableAir">' + '</td><td>' + PDB.planets[0].breathable + '</td></tr>';
+function infoUpdate(i){
+    let itemsTable = document.getElementById('info_table');
+    itemsTable.innerHTML = '';
+    itemsTable.innerHTML += '<th id="tellus"></th>' +
+    '<tr><td id="population">' + '</td><td>' + PDB.planets[i].population + '</td></tr>' +
+    '<tr><td id="starsystem">' + '</td><td id="' + PDB.planets[i].starsystem + '">' + '</td></tr>' +
+    '<tr><td id="infrastructure">' + '</td><td>' + PDB.planets[i].infrastructure + '</td></tr>' +
+    '<tr><td id="avgTemp">' + '</td><td>' + PDB.planets[i].meantemp + '</td></tr>' +
+    '<tr><td id="breathableAir">' + '</td><td>' + PDB.planets[i].breathable + '</td></tr>';
+    // Show the pane
+    document.getElementById('info').style.visibility = "visible";
+    update_dict_view();
 }
 
 /**
