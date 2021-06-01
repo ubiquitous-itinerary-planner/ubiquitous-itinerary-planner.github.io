a3ca79a0ca5bd5b8e32ec67aaf65aabe32c6c29c
tomvonsydow <tom@vonsydow.se>
2021-06-01 16:38:55 +0200

    tutorial rework

--- tutorial.js
+++ tutorial.js
@@ -1,59 +1,33 @@
+/**
+ * Create div over help button with a box shadow that darken the entire rest of the screen and starts tutorial mode.
+ */
 export function helpTutorial () {
-    //create help div for Help button + box shadow over entire screen
-    var help = document.createElement('div');
-    help.id = "helpDiv";
-    help.style.position = "absolute";
-    help.style.left = "186px";
-    help.style.top = "0px";
-    help.style.height = "50px";
-    help.style.width = "200px";
-    help.style.zIndex = "2";
-    help.style.backgroundColor = "black";
-    help.style.opacity = "25%";
-    help.style.boxShadow = "0px 0px 1px 2000px black";
+    //Create individual divs and applies non-general style
+    const parent = document.getElementById("tutorial");
+    createTutorialDiv(parent, "helpDiv", "186px", "0px", "50px", "200px");
+    createTutorialDiv(parent, "homeDiv", "0px", "0px", "50px", "200px");
+}
 
-    help.addEventListener("mouseover", mouseOver, false);
-    help.addEventListener("mouseout", mouseOut, false);
-    document.body.appendChild(help);
-    function mouseOver()
-    {
-        help.style.backgroundColor = "white";
-        help.style.opacity = "25%";
-        help.style.boxShadow = "";
-    }
-    function mouseOut()
-    {
-        help.style.backgroundColor = "";
-        help.style.opacity = "25%";
-    }
-
-    //Create home div (for star system button)
-    var home = document.createElement('div');
-    home.id = "homeDiv";
-    home.style.position = "absolute";
-    home.style.left = "0px";
-    home.style.top = "0px";
-    home.style.height = "50px";
-    home.style.width = "200px";
-    home.style.zIndex = "5";
-    //Hover function that lightens up home button. TO DO: Tooltip on hover
-    home.addEventListener("mouseover", mouseOver, false);
-    home.addEventListener("mouseout", mouseOut, false);
-    document.body.appendChild(home);
-    function mouseOver()
-    {
-        home.style.backgroundColor = "white";
-        home.style.opacity = "25%";
-        home.style.boxShadow = "";
-    }
-    function mouseOut()
-    {
-        home.style.backgroundColor = "";
-        home.style.opacity = "25%";
-        //home.style.boxShadow = "0px 0px 1px 2000px black";
-    }
-
-
-    document.getElementsByTagName('body')[0].appendChild(help);
-    document.getElementsByTagName('body')[0].appendChild(home);
+/**
+ * Creates tutorial divs based on style values from helpTutorial
+ * @param parent - Where we place the div
+ * @param id - The individual ids of tutorial "areas"
+ * @param left - div position left
+ * @param top - div position top
+ * @param height - div height
+ * @param width - div width
+ */
+function createTutorialDiv (parent, id, left, top, height, width){
+    //Create div element
+    const tutorialDiv = document.createElement('div');
+    //Assign id and class to div
+    tutorialDiv.id = id;
+    tutorialDiv.classList.add("help");
+    //Style div
+    tutorialDiv.style.left = left;
+    tutorialDiv.style.top = top;
+    tutorialDiv.style.height = height;
+    tutorialDiv.style.width = width;
+    //Add div to parent
+    parent.appendChild(tutorialDiv);
 }
